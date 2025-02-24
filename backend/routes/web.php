15cb5a264d8ac8032aaeef63f9af7f0db5dfe51f
maccceo <maceo.andreoli@gmail.com>
2025-02-24 10:46:00 +0100

    missing routes for Client and Product controllers

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -1,10 +1,14 @@
 <?php
 
+use App\Http\Controllers\ClientController;
 use App\Http\Controllers\OrderController;
+use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
+Route::apiResource('clients', ClientController::class);
+Route::apiResource('products', ProductController::class);
 Route::apiResource('orders', OrderController::class);
